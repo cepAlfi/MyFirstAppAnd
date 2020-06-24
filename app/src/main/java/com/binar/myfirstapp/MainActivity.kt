6c8062bcf1c05a3dc538602cf81b6b10dbd0b599
Andi Pratama Putra <andipp21@gmail.com>
2020-06-24 12:45:25 +0700

    commit

--- app/src/main/java/com/binar/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/binar/myfirstapp/MainActivity.kt
@@ -8,6 +8,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Commenan Baru Ini
 
+        //Komen apa lagi ya?
     }
 }
